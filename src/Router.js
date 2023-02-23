cf9573ace4e54e4aae3719f19bb846c4c540397d
Damian Goodenough <dgoodenough@proton.me>
2023-02-23 13:29:42 -0800

    creat about page and style buttons

--- src/Router.js
+++ src/Router.js
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 
 import Home from './components/Home';
+import AboutPage from './components/AboutPage';
 
 const Router = () => (
   <Routes>
     <Route exact path="/" element={<Home />} />
+    <Route exact path="/about" element={<AboutPage />} />
   </Routes>
 );
 
