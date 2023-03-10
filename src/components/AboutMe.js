fb1e4f986f3ca0cf6025bd149c6d0c2c0c1ef3bc
Damian Goodenough <dgoodenough@proton.me>
2023-03-09 16:43:24 -0800

    Open pdf file on click of resume and add bouncy arrow

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -4,68 +4,76 @@ import { Link } from 'react-router-dom';
 import FadeIn from './FadeIn';
 import profilePic from '../images/profile_pic.jpg';
 
-const AboutMe = () => (
-  <>
-    <FadeIn>
-      <Row className="text-center section-header">
-        <Col xs={12}>
-          <h1>Damian</h1>
-        </Col>
-        <h4>| Builder | Adventurer | Bitcoiner |</h4>
-      </Row>
-    </FadeIn>
-    <Col
-      className="d-flex align-items-center justify-content-center"
-      md={6}
-      xs={12}
-    >
+const AboutMe = () => {
+  const handleResumeClick = () => {
+    window.open('./resume.pdf');
+  };
+  return (
+    <>
       <FadeIn>
-        <img
-          src={profilePic}
-          alt="Profile portrait"
-          className="home-profile-pic"
-        />
+        <Row className="text-center section-header">
+          <Col xs={12}>
+            <h1>Damian</h1>
+          </Col>
+          <h4>| Builder | Adventurer | Bitcoiner |</h4>
+        </Row>
       </FadeIn>
-    </Col>
-    <Col className="d-flex align-items-center" md={6} xs={12}>
+      <Col
+        className="d-flex align-items-center justify-content-center"
+        md={6}
+        xs={12}
+      >
+        <FadeIn>
+          <img
+            src={profilePic}
+            alt="Profile portrait"
+            className="home-profile-pic"
+          />
+        </FadeIn>
+      </Col>
+      <Col className="d-flex align-items-center" md={6} xs={12}>
+        <FadeIn>
+          <div>
+            <p className="short-bio">
+              &emsp;Damian is a driven learner with a varied background that
+              provides a unique perspective to his work. He has developed
+              discipline through doing what no one else will.
+            </p>
+            <p>
+              &emsp;Damian is experienced with
+              <em> JavaScript</em>, skilled at building web applications using
+              MERN (<em>Mongo</em>, <em>Express</em>, <em>React</em>, and{' '}
+              <em>Node</em>) and has a deep understanding of <em>Bitcoin</em>{' '}
+              and it's technology. His goals and skills are highly motivated by
+              his desire to be great at what he does and build a better future.
+            </p>
+          </div>
+        </FadeIn>
+      </Col>
       <FadeIn>
-        <div>
-          <p className="short-bio">
-            &emsp;Damian is a driven learner with a varied background that
-            provides a unique perspective to his work. He has developed
-            discipline through doing what no one else will.
-          </p>
-          <p>
-            &emsp;Damian is experienced with
-            <em> JavaScript</em>, skilled at building web applications using
-            MERN (<em>Mongo</em>, <em>Express</em>, <em>React</em>, and{' '}
-            <em>Node</em>) and has a deep understanding of <em>Bitcoin</em> and
-            it's technology. His goals and skills are highly motivated by his
-            desire to be great at what he does and build a better future.
-          </p>
-        </div>
-      </FadeIn>
-    </Col>
-    <FadeIn>
-      <Row className="align-items-center buttons mt-3">
-        <Col className="home-button">
-          <Button variant="dark" as={Link} to="/resume">
-            Resumé
-          </Button>
-        </Col>
-        {/* <Col className="home-button">
+        <Row className="align-items-center buttons mt-3">
+          <Col className="home-button">
+            <Button onClick={handleResumeClick} variant="dark">
+              Resumé
+            </Button>
+          </Col>
+          <Col className="arrow bounce">
+            <i className="fa fa-arrow-down fa-2x" />
+          </Col>
+          {/* <Col className="home-button">
           <Button variant="dark" as={Link} to="/about">
             About Me
           </Button>
         </Col> */}
-        <Col className="home-button">
-          <Button variant="dark" as="a" href="#contact">
-            Contact
-          </Button>
-        </Col>
-      </Row>
-    </FadeIn>
-  </>
-);
+          <Col className="home-button">
+            <Button variant="dark" as="a" href="#contact">
+              Contact
+            </Button>
+          </Col>
+        </Row>
+      </FadeIn>
+    </>
+  );
+};
 
 export default AboutMe;
